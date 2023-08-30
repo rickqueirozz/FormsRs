0ee39e2e17164421e99423a91fc05e3904fe3f4e
Ricardo Queiroz <ricardo@MacBook-Air-de-Ricardo.local>
2023-08-29 22:05:14 -0300

    FormsRs

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 function obterDataHoraAtual() {
   const agora = new Date();
   const ano = agora.getFullYear();
-  const mes = String(agora.getMonth() + 1).padStart(2, '0'); 
+  const mes = String(agora.getMonth() + 1).padStart(2, '0');
   const dia = String(agora.getDate()).padStart(2, '0');
   const hora = String(agora.getHours()).padStart(2, '0');
   const minuto = String(agora.getMinutes()).padStart(2, '0');
   return `${ano}-${mes}-${dia}T${hora}:${minuto}`;
 }
 
-document.getElementById('dataHora').value = obterDataHoraAtual();
+function atualizarDataHora() {
+  const dataHoraInput = document.getElementById('dataHora');
+  dataHoraInput.value = obterDataHoraAtual();
+}
+
+atualizarDataHora();
+
+setInterval(atualizarDataHora, 60000); 
 
 function checkInputValidity(input) {
-  const value = input.valueAsNumber;
-  if (value > 999999) {
-    input.setCustomValidity("Digite no máximo 6 dígitos.");
-  } else {
+  const value = input.value;
+  if (value.length !== 6 || !/^\d+$/.test(value)) {
+    input.setCustomValidity("Digite exatamente 6 dígitos.");
+  } 
+  else {
     input.setCustomValidity("");
   }
 }
